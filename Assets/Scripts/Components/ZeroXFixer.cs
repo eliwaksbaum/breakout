d9d3320dd68ba26c17e98ece058c9fb4efd5d631
eliwaksbaum <62363023+eliwaksbaum@users.noreply.github.com>
2021-05-25 10:53:56 -0400

    ball shouldn't get stuck btwn block and ceiling anymore

--- Assets/Scripts/Components/ZeroXFixer.cs
+++ Assets/Scripts/Components/ZeroXFixer.cs
@@ -10,7 +10,11 @@ public class ZeroXFixer : MonoBehaviour
         {
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
 
-            if (Mathf.Abs(rb.velocity.x) < tolerance && rb.velocity.y < 0)
+            Debug.Log(rb.velocity.x);
+            Debug.Log(rb.velocity.y);
+
+            if (Mathf.Abs(rb.velocity.x) < tolerance && rb.velocity.y > 0) //Apparently the ball bounces, and
+                                                                            //changes directions before this is called
             {
                 float dir = (rb.velocity.x != 0)? rb.velocity.x/Mathf.Abs(rb.velocity.x) : 1;
                 rb.velocity = new Vector2(tolerance * dir, rb.velocity.y);
